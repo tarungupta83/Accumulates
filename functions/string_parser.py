6aa0c98db4ff26f31572b34edaef6d5688f43b88
Don <don_yin@outlook.com>
2022-01-10 16:15:37 +0000

    Update string_parser.py

--- functions/string_parser.py
+++ functions/string_parser.py
@@ -131,8 +131,8 @@ class string_parser:
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <div class="card flex-row flex-wrap">
-<img style="width:40%" src="data:image/png;base64,{image_data_bytes}" alt="cover">
-<div style="width:60%" class="card text-white bg-dark mb-3">
+<img style="width:25%" src="data:image/png;base64,{image_data_bytes}" alt="cover">
+<div style="width:75%" class="card text-white bg-dark mb-3">
     <div class="card-body">
     {string_parser.wrap_in_code_block(text=dict_file['file'], level='primary')}
     </div>
