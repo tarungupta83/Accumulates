5b3e1cc74c00134b670c2ab31c95a0afbf827013
Don Yin <Don_Yin@outlook.com>
2022-01-16 18:52:20 +0000

    Update string_parser.py

--- functions/string_parser.py
+++ functions/string_parser.py
@@ -65,8 +65,6 @@ class string_parser:
                 string_parser.wrap_content_in_color_span(text=i.replace(" ", "-"), color="Gold"), content
             )
 
-            # content = content.replace(i, string_parser.wrap_content_in_color_span(text=i, color="Gold"))
-
         for i in keywords:
             compiler = re.compile(re.escape(i), re.IGNORECASE)
             content = compiler.sub(string_parser.wrap_content_in_color_span(text=i, color="Tomato"), content)
@@ -163,8 +161,8 @@ class string_parser:
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <div class="card flex-row flex-wrap">
-<img style="width:25%" src="data:image/png;base64,{image_data_bytes}" alt="cover">
-<div style="width:75%" class="card text-white bg-dark mb-3">
+<img style="width:35%" src="data:image/png;base64,{image_data_bytes}" alt="cover">
+<div style="width:65%" class="card text-white bg-dark mb-3">
     <div class="card-body">
     {string_parser.wrap_in_code_block(text=dict_file['file'], level='primary')}
     </div>
