2618128ee473506a1002740f136e68704347eaa3
Don <don_yin@outlook.com>
2022-01-17 16:04:50 +0000

    Update string_parser.py

--- functions/string_parser.py
+++ functions/string_parser.py
@@ -159,16 +159,13 @@ class string_parser:
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <div class="card flex-row flex-wrap">
-<img style="width:35%" src="data:image/png;base64,{image_data_bytes}" alt="cover">
-<div style="width:65%" class="card text-white bg-dark mb-3">
+<img style="width:32%" src="data:image/png;base64,{image_data_bytes}" alt="cover">
+<div style="width:68%" class="card text-white bg-dark mb-3">
     <div class="card-body">
     {string_parser.wrap_in_code_block(text=dict_file['file'], level='primary')}
     </div>
     <div class="card-body" style="line-height: 200%;">
     {dict_language['metadata_subject']} {string_parser.wrap_in_code_block(text=str(doc.metadata['subject']), level='secondary')}
-    </div>
-    <div class="card-body" style="line-height: 200%;">
-    {dict_language['note_count']} {string_parser.wrap_in_code_block(text=str(dict_file['count_note']), level='secondary')}
     </div>
     <div class="card-body" style="line-height: 200%;">
     {dict_language['percentage_pages_noted']}
@@ -186,3 +183,7 @@ class string_parser:
 </div>
 <br>
 """
+
+    # <div class="card-body" style="line-height: 200%;">
+    # {dict_language['note_count']} {string_parser.wrap_in_code_block(text=str(dict_file['count_note']), level='secondary')}
+    # </div>
